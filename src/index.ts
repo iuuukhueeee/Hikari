04cb907b1006e871062f4a5262d715ee1f4fa83c
iuuukhueeee <nguyenducthien9@gmail.com>
2025-08-08 15:21:03 +0700

    feat: binding d1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Hono } from 'hono'
+import prismaClients from './lib/prisma'
 
-const app = new Hono()
+type Bindings = {
+  DB: D1Database
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/', async (c) => {
+  const prisma = await prismaClients.fetch(c.env.DB)
+  const users = await prisma.user.findMany()
+
+  return c.json(users)
 })
 
 export default app
