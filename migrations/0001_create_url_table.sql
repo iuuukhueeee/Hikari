02142296a890e06c859586a0c1659cda003c2350
iuuukhueeee <nguyenducthien9@gmail.com>
2025-08-08 17:07:50 +0700

    feat: add short url routing

---
+++ migrations/0001_create_url_table.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Url" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "originalUrl" TEXT NOT NULL,
+    "shortUrl" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Url_originalUrl_key" ON "Url"("originalUrl");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Url_shortUrl_key" ON "Url"("shortUrl");
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import prismaClients from './lib/prisma'
+import generateSha256Hash from './lib/generateSha256Hash'
 
 type Bindings = {
   DB: D1Database
@@ -12,6 +13,42 @@ app.get('/', async (c) => {
   const users = await prisma.user.findMany()
 
   return c.json(users)
+})
+
+app.get('/redirect', (c) => {
+  return c.redirect('https://yusou.dev')
+})
+
+app.post('/', async (c) => {
+  const prisma = await prismaClients.fetch(c.env.DB)
+  const { originalUrl, shortUrl, description } = await c.req.json()
+
+  const url = await prisma.url.create({
+    data: {
+      originalUrl: originalUrl,
+      shortUrl: shortUrl ?? generateSha256Hash(originalUrl),
+      description: description ?? null,
+    },
+  })
+
+  return c.json(url)
+})
+
+app.get('/:url', async (c) => {
+  const url = c.req.param('url')
+  const prisma = await prismaClients.fetch(c.env.DB)
+
+  const foundUrl = await prisma.url.findUnique({
+    where: {
+      shortUrl: url,
+    },
+  })
+
+  if (!foundUrl) {
+    return c.text('URL not found', 404)
+  }
+
+  return c.redirect(foundUrl.originalUrl)
 })
 
 export default app
